e50a6ecab1d2ca4134f7f0030bbbc77cd19f643d
Teodor Genov <teodorgenov06@gmail.com>
2026-01-19 18:02:21 +0200

    Removed unused includes

--- main.cpp
+++ main.cpp
@@ -14,12 +14,7 @@
  */
 
 #include <iostream>
-#include <vector>
 // #include <fstream>
-#include <algorithm>
-#include <random>
-#include <windows.h>
-#include <cctype>
 #include <cstring>
 
 #include "utils.hpp"
